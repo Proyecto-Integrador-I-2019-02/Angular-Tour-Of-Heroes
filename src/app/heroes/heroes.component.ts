ce717017b288101b8f8a2c4e9d3583a6eeb397d3
Steven <steven.deibi@hotmail.com>
2019-12-06 15:18:06 -0500

    5. Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,20 +9,15 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
-    
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-  this.getHeroes();
-}   
-    
-  onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+    this.getHeroes();
+  }
 
-   getHeroes(): void {
-  this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-}
-    
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
